8856cc792b433f848628c01137147f1c21166dce
Sanchit98 <sanchit@novolytics.co>
2023-02-19 22:16:08 +0530

    first commit

---
+++ src/About.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import HeroSection from './components/HeroSection'
+import { useProductContext } from './context/productcontext';
+
+const About = () => {
+  const {myName} = useProductContext();
+  const data = {
+    name: "Ecommerce"
+  }
+  return (
+    <>
+    {myName}
+    <HeroSection myData={data}/>
+    </>
+  )
+}
+
+export default About
